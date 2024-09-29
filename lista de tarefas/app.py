179d3760a8094703bf651a51578a1703e2fca774
Enzo Maximino de Sousa Costa <enzomaximino2007@gmail.com>
2024-09-28 21:53:36 -0300

    Delete lista de tarefas directory

--- lista de tarefas/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from controllers import restaurantes_controller
-
-
-app = Flask(__name__)
-app.register_blueprint(restaurantes_controller)
-
-if __name__ == "__main__":
-    app.run()
